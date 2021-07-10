a97bc1473ad306090e8e5e98fd02868de68bdc4e
Jeff Bachtel <jeff.bachtel@mariadb.com>
2021-07-10 21:56:42 +0000

    pass in lambda stack to pipeline stack

--- pipeline/app.py
+++ pipeline/app.py
@@ -10,25 +10,27 @@ from aws_cdk import core as cdk
 from aws_cdk import core
 
 from pipeline.pipeline_stack import PipelineStack
+from pipeline.lambda_stack import LambdaStack
 
 
 app = core.App()
-PipelineStack(app, "PipelineStack",
+
+lambda_stack = LambdaStack(app, "LambdaStack")
+
+PipelineStack(
+    app,
+    "PipelineStack",
+    lambda_code=lambda_stack.lambda_code
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
-
     # Uncomment the next line to specialize this stack for the AWS Account
     # and Region that are implied by the current CLI configuration.
-
-    #env=core.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
+    # env=core.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
-
-    #env=core.Environment(account='123456789012', region='us-east-1'),
-
+    # env=core.Environment(account='123456789012', region='us-east-1'),
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+)
 
 app.synth()
