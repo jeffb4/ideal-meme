88b75b66ce1b94edd83b86814516d68a40c370d7
Jeff Bachtel <jeff.bachtel@mariadb.com>
2021-07-10 23:22:29 +0000

    do the assignment

--- pipeline/pipeline/lambda/index.ts
+++ pipeline/pipeline/lambda/index.ts
@@ -1,6 +1,10 @@
 // index.ts
-const GREETING = "Hello, AWS!";
+const MESSAGE = {
+  message: "Automate all the things!",
+  secretMessage: "Automation for the people!",
+  timestamp: Math.round(Date.now() / 1000),
+};
 export async function main(event: any, context: any) {
-  console.log(GREETING);
-  return GREETING;
+  console.log(MESSAGE);
+  return MESSAGE;
 }
