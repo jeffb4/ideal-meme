4f37007f3a44de6efd649c186c8b34df7e24e161
Jeff Bachtel <jeff.bachtel@mariadb.com>
2021-07-11 15:58:26 -0600

    Add endpoint discovery to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ "${GH_TOKEN}" = "" ]; then
   echo "GH_TOKEN environment variable must be set to a GitHub Personal Access Token"
 fi
+
+export AWS_REGION=us-east-1
+export AWS_DEFAULT_REGION=$AWS_REGION
 
 cd /app/pipeline || exit 1
 
@@ -10,3 +13,11 @@ cdk deploy \
   --require-approval never \
   --parameters "PipelineStack:githubToken=${GH_TOKEN}" \
   PipelineStack
+
+API_ENDPOINT=$(aws --region $AWS_REGION cloudformation describe-stacks --stack-name LambdaDeploymentStack | jq -r ".Stacks[0].Outputs[0].OutputValue")
+
+while [ "$API_ENDPOINT" = "" ]; do
+  API_ENDPOINT=$(aws --region $AWS_REGION cloudformation describe-stacks --stack-name LambdaDeploymentStack | jq -r ".Stacks[0].Outputs[0].OutputValue")
+done
+
+echo "API should be available at $API_ENDPOINT"
