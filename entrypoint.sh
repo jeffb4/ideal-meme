5ba7b9ddb91d4d6e8ed8809dd56e029f781091ba
Jeff Bachtel <jeff.bachtel@mariadb.com>
2021-07-11 03:19:05 +0000

    require gh token be passed in to container

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if [ "${GH_TOKEN}" = "" ]; then
+  echo "GH_TOKEN environment variable must be set to a GitHub Personal Access Token"
+fi
+
 cd /app/pipeline || exit 1
 
-cdk deploy
+cdk deploy \
+  --parameters "PipelineStack:githubToken=${GH_TOKEN}" \
+  PipelineStack
